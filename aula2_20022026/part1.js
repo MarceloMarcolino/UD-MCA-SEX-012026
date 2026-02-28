6e775d9aa8ecca13020fcad117712ebc0d836265
kauahenr1 <kaua.gazarolis@icloud.com>
2026-02-27 21:49:37 -0300

    atividade2

--- aula2_20022026/part1.js
+++ aula2_20022026/part1.js
@@ -1,67 +1,67 @@
 // Parte 1 – Array Simples
-// Array com pelo menos 20 elementos diferentes (nomes de jogos)
+// Array com pelo menos 20 elementos diferentes (nomes de times de futebol)
 
-let jogos = [
-    'Minecraft',           // 0
-    'GTA V',               // 1
-    'Tetris',              // 2
-    'The Witcher 3',       // 3
-    'Red Dead Redemption 2', // 4
-    'God of War',          // 5
-    'Elden Ring',          // 6
-    'Fortnite',            // 7
-    'Zelda: Breath of the Wild', // 8
-    'Super Mario Odyssey', // 9
-    'Resident Evil 4',     // 10
-    'Counter-Strike 2',    // 11
-    'League of Legends',   // 12
-    'Valorant',            // 13
-    'Overwatch 2',         // 14
-    'FIFA 24',             // 15
-    'Halo Infinite',       // 16
-    'Cyberpunk 2077',      // 17
-    'Dark Souls 3',        // 18
-    'Bloodborne',          // 19
-    'Sekiro',              // 20
+let times = [
+    'Flamengo',            // 0
+    'Palmeiras',           // 1
+    'São Paulo',           // 2
+    'Corinthians',         // 3
+    'Santos',              // 4
+    'Grêmio',              // 5
+    'Internacional',       // 6
+    'Atlético Mineiro',    // 7
+    'Cruzeiro',            // 8
+    'Vasco',               // 9
+    'Botafogo',            // 10
+    'Bahia',               // 11
+    'Fortaleza',           // 12
+    'Ceará',               // 13
+    'Fluminense',          // 14
+    'Athletico Paranaense',// 15
+    'Bragantino',          // 16
+    'Juventude',           // 17
+    'Sport',               // 18
+    'Chapecoense',         // 19
+    'Coritiba'             // 20
 ];
 
 // =====================================================
 // A. Qual elemento está na posição 0, 7, 11, 15, 18 e 20?
 // =====================================================
 console.log('=== Pergunta A ===');
-console.log('Posição 0:', jogos[0]);   // Minecraft
-console.log('Posição 7:', jogos[7]);   // Fortnite
-console.log('Posição 11:', jogos[11]); // Counter-Strike 2
-console.log('Posição 15:', jogos[15]); // FIFA 24
-console.log('Posição 18:', jogos[18]); // Dark Souls 3
-console.log('Posição 20:', jogos[20]); // Sekiro
+console.log('Posição 0:', times[0]);
+console.log('Posição 7:', times[7]);
+console.log('Posição 11:', times[11]);
+console.log('Posição 15:', times[15]);
+console.log('Posição 18:', times[18]);
+console.log('Posição 20:', times[20]);
 
 // =====================================================
 // B. Qual elemento está na penúltima e última posição?
 // =====================================================
 console.log('\n=== Pergunta B ===');
-console.log('Penúltima posição (' + (jogos.length - 2) + '):', jogos[jogos.length - 2]); // Bloodborne
-console.log('Última posição (' + (jogos.length - 1) + '):', jogos[jogos.length - 1]);     // Sekiro
+console.log('Penúltima posição (' + (times.length - 2) + '):', times[times.length - 2]);
+console.log('Última posição (' + (times.length - 1) + '):', times[times.length - 1]);
 
 // =====================================================
 // C. Quantos elementos existem no array?
 // =====================================================
 console.log('\n=== Pergunta C ===');
-console.log('Quantidade de elementos:', jogos.length); // 21
+console.log('Quantidade de elementos:', times.length);
 
 // =====================================================
 // D. Adicione um novo elemento ao final do array.
 // =====================================================
 console.log('\n=== Pergunta D ===');
-jogos.push('Horizon Forbidden West');
-console.log('Novo elemento adicionado: Horizon Forbidden West');
-console.log('Nova quantidade de elementos:', jogos.length); // 22
+times.push('Seleção Brasileira');
+console.log('Novo elemento adicionado: Seleção Brasileira');
+console.log('Nova quantidade de elementos:', times.length);
 
 // =====================================================
 // E. Imprima todos os elementos usando um for.
 // =====================================================
 console.log('\n=== Pergunta E ===');
 console.log('Todos os elementos do array:');
-for (let i = 0; i < jogos.length; i++) {
-    console.log('Posição ' + i + ': ' + jogos[i]);
+for (let i = 0; i < times.length; i++) {
+    console.log('Posição ' + i + ': ' + times[i]);
 }
